47301493f163df6d243520c94d0b895715766214
Nikita Sychev <root@nsychev.ru>
2020-06-18 19:28:54 +0500

    fix photo downloader

--- app/decorators.py
+++ app/decorators.py
@@ -30,16 +30,17 @@ def attach_user(func: TelegramAction) -> TelegramAction:
             force_insert = True
 
         try:
-            avatar = context.bot.get_user_profile_photos(user.id, offset=None, limit=1)[0][0]
+            avatar = context.bot.get_user_profile_photos(user.id, offset=0, limit=1)
+            avatar = avatar.photos[0][0]
             if context.user.avatar_id != avatar.file_unique_id:
                 context.user.avatar_id = avatar.file_unique_id
-                avatar.get_file.download(
+                avatar.get_file().download(
                     os.path.join(
                         os.path.dirname(os.path.realpath(__file__)),
-                        'static', user.id
+                        'static', f'{user.id}.jpg'
                     )
                 )                
-        except:
+        except KeyError:
             context.user.avatar_id = None
             
         context.user.id = user.id
