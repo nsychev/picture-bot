ec590a530cf6d0d6a9f8b58e2a8d14ae8caacde9
Vanya Klimenko <k60+git@fmap.me>
2020-06-21 20:43:49 +0300

    test matplotlib with flask and colors

--- app/routes.py
+++ app/routes.py
@@ -6,6 +6,10 @@ from telegram import Bot, Update
 import config
 from models import database, User, Post
 
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.figure import Figure
+import random
+from io import BytesIO
 
 def create_app(bot: Bot, update_queue: Queue) -> flask.Flask:
     # pylint: disable=unused-variable
@@ -34,6 +38,31 @@ def create_app(bot: Bot, update_queue: Queue) -> flask.Flask:
             'position': idx + 1
         } for idx, user in enumerate(users_query) if user.count > 0]
         return flask.render_template('top.html', users=users)
+
+    @app.route('/grafik')
+    def plot():
+            fig = Figure(figsize=(16, 10))
+            axis = fig.add_subplot(1, 1, 1)
+
+            for i in range(10):
+                xs = range(100)
+                ys = [random.randint(1, 50) for x in xs]
+                axis.plot(xs, ys)
+
+            axis.set(
+                xlabel="эшельме",
+                ylabel="бэшельме",
+                title="Հայոց պատմության 10 դասարան" 
+            )
+            return respond_with_figure(fig)
+
+    def respond_with_figure(figure):
+            canvas = FigureCanvas(figure)
+            output = BytesIO()
+            canvas.print_png(output)
+            response = flask.make_response(output.getvalue())
+            response.mimetype = 'image/png'
+            return response
 
     return app
 
